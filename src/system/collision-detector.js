c247112262185db475ad2be615aac2375a2071ac
romulospl <romulopereiralopes@gmail.com>
2024-03-28 18:17:21 -0300

    feat: detector de colisão

---
+++ src/system/collision-detector.js
@@ -0,0 +1,28 @@
+import { showLog } from './showLogs'
+
+AFRAME.registerComponent('collision', {
+    schema: {
+        area: { default: '' },
+    },
+    tick: function () {
+        if (this.data.area == '') return
+        var areaTridimensional = document.querySelector(this.data.area);
+        var objetoPosicao = this.el.object3D.position.clone(); // Posição do objeto
+        var areaTridimensionalPosicao = areaTridimensional.object3D.position.clone(); // Posição da área tridimensional
+        var areaTridimensionalEscala = areaTridimensional.getAttribute('scale').clone(); // Escala da área tridimensional
+
+        if (
+            objetoPosicao.x >= areaTridimensionalPosicao.x - areaTridimensionalEscala.x / 2 &&
+            objetoPosicao.x <= areaTridimensionalPosicao.x + areaTridimensionalEscala.x / 2 &&
+            objetoPosicao.y >= areaTridimensionalPosicao.y - areaTridimensionalEscala.y / 2 &&
+            objetoPosicao.y <= areaTridimensionalPosicao.y + areaTridimensionalEscala.y / 2 &&
+            objetoPosicao.z >= areaTridimensionalPosicao.z - areaTridimensionalEscala.z / 2 &&
+            objetoPosicao.z <= areaTridimensionalPosicao.z + areaTridimensionalEscala.z / 2
+        ) {
+            this.el.emit('collisionstarted'); // emite o inicio da colisão
+        } else {
+            // this.el.emit('isnotcolision');
+            this.el.emit('collisionended'); // emite o fim da colisão
+        }
+    }
+});
