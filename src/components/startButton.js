41c5c2b326124dde6f14c5c588f99762d5aa35bc
romulospl <romulopereiralopes@gmail.com>
2024-03-28 19:33:18 -0300

    change light

--- src/components/startButton.js
+++ src/components/startButton.js
@@ -1,24 +1,37 @@
 
 import { showLog } from '../system/showLogs'
+import { manipuladorObjects } from '../util/manipuladorObjects'
 
 AFRAME.registerComponent('start-button', {
     schema: {
     },
     init: function () {
-        this.bindMethods()
-        this.el.addEventListener('grab-start', this.hoverStart)
+        try {
+            this.manipulador = new manipuladorObjects(this.el)
+
+            this.bindMethods()
+            this.el.addEventListener('grab-start', this.hoverStart)
+        } catch (error) {
+            showLog(error)
+        }
+
+
     },
     bindMethods: function () {
         this.hoverStart = this.hoverStart.bind(this)
     },
     hoverStart: function () {
-        showLog("pressionado")
-        // let message = document.querySelector('#desafioconclusion')
-        // message.setAttribute('visible', 'false')
-        // this.manipulador.setPosition('0 -10 0')
-        // this.modelButton.setPosition('0 -10 0')
-        // this.modelRestartButton.setPosition('0 -10 0')
-        // this.el.emit('startgame')
+        try {
+            let manipuladorButtonModel = new manipuladorObjects(document.querySelector('#button-start'))
+
+            manipuladorButtonModel.setPosition('0 -10 0')
+            this.manipulador.setPosition('0 -10 0')
+            showLog("pressionado")
+
+            this.el.emit('startgame')
+        } catch (error) {
+
+        }
     },
 });
 
