c5b3a824a7d1a7a966ab15493abefaf1e468f313
romulospl <romulopereiralopes@gmail.com>
2024-03-29 09:58:34 -0300

    atualização build

--- Dockerfile
+++ Dockerfile
@@ -9,3 +9,4 @@ RUN npm run build
 FROM nginx:alpine
 COPY --from=build /app/dist /usr/share/nginx/html
 EXPOSE 80
+EXPOSE 443
--- deploy-local.sh
+++ deploy-local.sh
@@ -10,3 +10,5 @@ docker rm -f sustentabilidade-vr
 docker build -t sustentabilidade .
 
 docker run -p 80:80 -p 443:443 -d --name sustentabilidade-vr sustentabilidade
+
+
