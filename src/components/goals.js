5ffbe86098f7bd09b3a8002fbab35aba3bebebba
romulospl <romulopereiralopes@gmail.com>
2024-03-29 22:38:49 -0300

    inserção do lado direito do game

--- src/components/goals.js
+++ src/components/goals.js
@@ -1,7 +1,8 @@
 import { ManipuladorObjects } from '@/util/manipuladorObjects'
 import { showLog, showLog2 } from '@/system/showLogs'
 
-const posicaoInicial = "0 1.7 -63.14484"
+// const posicaoInicial = "0 1.7 -63.14484"
+const posicaoInicial = "0 1.7 -44.78514"
 const posicaoCentralizada = "0.03156 1.3189 -0.83031"
 const tempoDeAnimacao = 8000
 
@@ -45,12 +46,10 @@ function sortearGoal(el) {
     return { sorteado: document.getElementById(elementoAleatorio.id), value: getInfoModelo(elementoAleatorio.id).value }
 }
 
-// function getValueModelo(el) {
-//     let modelo = new ManipuladorObjects(el)
-//     const regex = /(-left|-right)/g;
-//     let elementoId = modelo.getElementId().replace(regex, "")
-//     return getInfoModelo(elementoId).value
-// }
+function getTempoAleatorio() {
+    return (Math.floor(Math.random() * (3 - 1 + 1)) + 1) * 1000;
+  }
+
 
 /*
 STATUS:
@@ -84,7 +83,7 @@ AFRAME.registerComponent('goal-object-left', {
         try {
             this.manipulador = new ManipuladorObjects(this.el)
             this.posicaoAlvo = "-1.12442 1.42 4.935"
-            this.modelosEscolhidos = []
+            this.modelosEscolhidosLeft = []
 
             this.manipuladorScape = new ManipuladorObjects(document.querySelector('#scape-left'))
 
@@ -123,7 +122,7 @@ AFRAME.registerComponent('goal-object-left', {
     iniciarJogo: function () {
         if (this.status === 'pronto') {
             this.selecionarModelo()
-            this.adicionarAnimacao()
+            setTimeout(() => this.adicionarAnimacao(), getTempoAleatorio())
             this.status = 'iniciado'
         }
     },
@@ -131,7 +130,7 @@ AFRAME.registerComponent('goal-object-left', {
         // console.clear();
         let modeloSorteado = sortearGoal(this.el)
         let manipuladorModeloSorteado = new ManipuladorObjects(modeloSorteado.sorteado)
-        while (this.modelosEscolhidos.includes(manipuladorModeloSorteado.getElementId())) {
+        while (this.modelosEscolhidosLeft.includes(manipuladorModeloSorteado.getElementId())) {
             modeloSorteado = sortearGoal(this.el)
             manipuladorModeloSorteado = new ManipuladorObjects(modeloSorteado.sorteado)
         }
@@ -140,7 +139,7 @@ AFRAME.registerComponent('goal-object-left', {
         this.pontuacao = modeloSorteado.value
 
         // console.log(`pontuação: ${this.modeloSorteado}`)
-        this.modelosEscolhidos.push(manipuladorModeloSorteado.getElementId())
+        this.modelosEscolhidosLeft.push(manipuladorModeloSorteado.getElementId())
         let infoModelo = getInfoModelo(manipuladorModeloSorteado.getElementId())
         if (manipuladorModeloSorteado.getPosition("y") < -2) {
             manipuladorModeloSorteado.setPosition(`${manipuladorModeloSorteado.getPosition("x")} ${infoModelo.altura} ${manipuladorModeloSorteado.getPosition("z")}`)
@@ -222,22 +221,159 @@ AFRAME.registerComponent('goal-object-left', {
         console.log("Fim de jogo")
     },
     tick: function () {
-        if (this.modelosEscolhidos.length > 7) this.modelosEscolhidos = []
+        if (this.modelosEscolhidosLeft.length > 7) this.modelosEscolhidosLeft = []
     }
 });
-
 
 AFRAME.registerComponent('goal-object-right', {
     schema: {
         toPosition: { default: '0 0 16.15' },
     },
     init: function () {
+        try {
+            this.manipulador = new ManipuladorObjects(this.el)
+            this.posicaoAlvo = "1.12442 1.42 4.935"
+            this.modelosEscolhidosRight = []
 
+            this.manipuladorScape = new ManipuladorObjects(document.querySelector('#scape-right'))
+
+            this.irParaPosicaoInicial()
+
+            let buttonStart = document.querySelector('#button-start-contato')
+
+            this.caixaSustentavel = document.querySelector('#caixote-sustentavel-right')
+            this.caixaNaoSustentavel = document.querySelector('#caixote-nao-sustentavel-right')
+
+            this.bindMethods()
+            this.el.addEventListener('collisionstarted', this.verificarAreaEscape)
+            this.el.addEventListener('hover-start', this.hoverStart)
+            buttonStart.addEventListener('startgame', this.iniciarJogo)
+        } catch (error) {
+            showLog(error)
+            console.log(error)
+        }
     },
     bindMethods: function () {
-
+        this.iniciarJogo = this.iniciarJogo.bind(this)
+        this.selecionarModelo = this.selecionarModelo.bind(this)
+        this.irParaPosicaoInicial = this.irParaPosicaoInicial.bind(this)
+        this.irParaAreaCentralizada = this.irParaAreaCentralizada.bind(this)
+        this.adicionarAnimacao = this.adicionarAnimacao.bind(this)
+        this.removerAnimacao = this.removerAnimacao.bind(this)
+        this.verificarAreaEscape = this.verificarAreaEscape.bind(this)
+        this.resetarOpcoesCaixote = this.resetarOpcoesCaixote.bind(this)
+        this.removerModelo = this.removerModelo.bind(this)
+        this.reiniciarGoal = this.reiniciarGoal.bind(this)
+        this.hoverStart = this.hoverStart.bind(this)
+        this.verificarCaixotePositivo = this.verificarCaixotePositivo.bind(this)
+        this.verificarCaixoteNegativo = this.verificarCaixoteNegativo.bind(this)
+        this.endGame = this.endGame.bind(this)
     },
+    iniciarJogo: function () {
+        if (this.status === 'pronto') {
+            this.selecionarModelo()
+            setTimeout(() => this.adicionarAnimacao(), getTempoAleatorio())
+            this.status = 'iniciado'
+        }
+    },
+    selecionarModelo: function () {
+        // console.clear();
+        let modeloSorteado = sortearGoal(this.el)
+        let manipuladorModeloSorteado = new ManipuladorObjects(modeloSorteado.sorteado)
+        while (this.modelosEscolhidosRight.includes(manipuladorModeloSorteado.getElementId())) {
+            modeloSorteado = sortearGoal(this.el)
+            manipuladorModeloSorteado = new ManipuladorObjects(modeloSorteado.sorteado)
+        }
 
+        this.modeloSorteado = modeloSorteado.sorteado
+        this.pontuacao = modeloSorteado.value
+
+        // console.log(`pontuação: ${this.modeloSorteado}`)
+        this.modelosEscolhidosRight.push(manipuladorModeloSorteado.getElementId())
+        let infoModelo = getInfoModelo(manipuladorModeloSorteado.getElementId())
+        if (manipuladorModeloSorteado.getPosition("y") < -2) {
+            manipuladorModeloSorteado.setPosition(`${manipuladorModeloSorteado.getPosition("x")} ${infoModelo.altura} ${manipuladorModeloSorteado.getPosition("z")}`)
+        }
+    },
+    removerModelo: function () {
+        let manipuladorModeloSorteado = new ManipuladorObjects(this.modeloSorteado)
+        if (manipuladorModeloSorteado.getPosition("y") > -2) {
+            manipuladorModeloSorteado.setPosition(`${manipuladorModeloSorteado.getPosition("x")} ${manipuladorModeloSorteado.getPosition("y") - 10} ${manipuladorModeloSorteado.getPosition("z")}`)
+        }
+    },
+    irParaPosicaoInicial: function () {
+        this.status = 'pronto'
+        this.el.addEventListener('hover-start', this.hoverStart)
+        this.manipulador.setPosition(posicaoInicial)
+    },
+    adicionarAnimacao: function () {
+        this.removerAnimacao()
+        console.log(this.posicaoAlvo)
+        this.manipulador.addAnimation(this.posicaoAlvo, tempoDeAnimacao)
+    },
+    removerAnimacao: function () {
+        this.manipulador.deleteAnimation()
+    },
+    verificarAreaEscape: function () {
+        this.reiniciarGoal()
+    },
+    reiniciarGoal: function () {
+        this.removerModelo()
+        this.irParaPosicaoInicial()
+        setTimeout(() => {
+            if (pontuacao >= pontuacaoFinal) {
+                this.endGame()
+                return
+            } else {
+                this.iniciarJogo()
+            }
+        }, 500)
+    },
+    hoverStart: function () {
+        if (this.status === 'iniciado') this.irParaAreaCentralizada()
+    },
+    irParaAreaCentralizada: function () {
+        this.el.removeEventListener('hover-start', this.hoverStart)
+        this.status = 'hoverStart'
+        let time = 2000
+        try {
+            this.removerAnimacao()
+            this.manipulador.addAnimation(posicaoCentralizada, time)
+            this.manipulador.removeAttribute('hoverable')
+            setTimeout(() => {
+                this.caixaSustentavel.addEventListener('collisionstarted', this.verificarCaixotePositivo)
+                this.caixaNaoSustentavel.addEventListener('collisionstarted', this.verificarCaixoteNegativo)
+                this.manipulador.addAttribute('grabbable')
+                this.removerAnimacao()
+            }, (time + 500))
+        } catch (error) {
+            showLog(error)
+        }
+    },
+    verificarCaixotePositivo: function () {
+        this.resetarOpcoesCaixote()
+        if (this.pontuacao == 1) {
+            pontuacao++
+        } else {
+            showLog("Errou")
+        }
+    },
+    verificarCaixoteNegativo: function () {
+        this.resetarOpcoesCaixote()
+    },
+    resetarOpcoesCaixote: function () {
+        this.caixaSustentavel.removeEventListener('collisionstarted', this.verificarCaixotePositivo)
+        this.caixaNaoSustentavel.removeEventListener('collisionstarted', this.verificarCaixoteNegativo)
+        this.reiniciarGoal()
+    },
+    endGame: function () {
+        showLog("Desafio concluído", 'green')
+        console.clear()
+        console.log("Fim de jogo")
+    },
+    tick: function () {
+        if (this.modelosEscolhidosRight.length > 7) this.modelosEscolhidosRight = []
+    }
 });
 
 
